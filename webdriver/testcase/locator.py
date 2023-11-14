1da71159b577ae58b72c0bc77997b3c2a5d9da0e
phamtungthuy <21020412@vnu.edu.vn>
2023-11-14 11:05:10 +0700

    Create tests case to edit information of account:

--- webdriver/testcase/locator.py
+++ webdriver/testcase/locator.py
@@ -9,9 +9,31 @@ class SearchResultsPageLocators(object):
 class LumaMainPageLocators(object):
     SIGNUP_LINK = (By.LINK_TEXT, "Create an Account")
     LOGIN_LINK = (By.CSS_SELECTOR, ".authorization-link a")
+    SWITCH_BUTTON = (By.CLASS_NAME, "switch")
+    ACCOUNT_LINK = (By.LINK_TEXT, "My Account")
 
 class LumaSignupPageLocators(object):
     SIGNUP_BUTTON = (By.CLASS_NAME, "submit")
 
 class LumaLoginPageLocators(object):
     LOGIN_BUTTON = (By.CSS_SELECTOR, "#send2.login")
+
+class LumaAccountPageLocators(object):
+    EDIT_ADDRESS = (By.LINK_TEXT, "Edit Address")
+    EDIT_INFORMATION = (By.CSS_SELECTOR, ".box-information .box-actions .edit")
+    MANAGE_ADDRESS = (By.CSS_SELECTOR, ".items .item:nth-child(6)")
+
+class LumaEditPageLocators(object):
+    UPDATE_BUTTON = (By.CLASS_NAME, "save")
+    PASSWORD_CHECKBOX = (By.ID, "change-password")
+
+class LumaEditAddressPageLocators(object):
+    UPDATE_BUTTON = (By.CLASS_NAME, "save")
+    
+class LumaManageAddressPageLocators(object):
+    CREATE_BUTTON = (By.CLASS_NAME, "add")
+    DELETE_BUTTON = (By.CLASS_NAME, "delete")
+    ACCEPT_BUTTON = (By.CLASS_NAME, "action-accept")
+
+class LumaCreateAddressPageLocators(object):
+    CREATE_BUTTON = (By.CLASS_NAME, "save")
--- webdriver/testcase/test.py
+++ webdriver/testcase/test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from . import page
 from . import *
 import time
-
+import random
 class LumaWebsiteAuthentication(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(options=options)
@@ -66,7 +66,103 @@ class LumaWebsiteAuthentication(unittest.TestCase):
 
         assert loginPage.is_login_successful()
         time.sleep(5)
+
+    
+
     def tearDown(self):
         self.driver.close()
 
+class LumaControlAccount(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get(luma_website)
+        mainpage = page.LumaMainPage(self.driver)
+        mainpage.go_to_login_page()
 
+        loginPage = page.LumaLoginPage(self.driver)
+        loginPage.emailAddress = "selenium@selenium.com"
+        loginPage.password = "Password123"
+        loginPage.login()
+
+        mainpage.go_to_account_page()
+
+
+    def test_edit_name(self):
+        
+        accountPage = page.LumaAccountPage(self.driver)
+        assert accountPage.is_title_matches()
+        accountPage.go_to_edit_account_information_page()
+        
+        editPage = page.LumaEditPage(self.driver)
+        assert editPage.is_title_matches()
+        editPage.turn_on_edit_password()
+        editPage.firstName = "Marry"
+        editPage.lastName = "Queen"
+        editPage.currentPassword = "Password123"
+        editPage.password = "Password123"
+        editPage.confirmPassword = "Password123"
+
+        editPage.update_account_information()
+
+        assert editPage.is_updated_successfully()
+
+    def test_edit_address(self):
+
+        accountPage = page.LumaAccountPage(self.driver)
+        assert accountPage.is_title_matches()
+        accountPage.go_to_edit_address_page()
+
+        editAddressPage = page.LumaEditAddressPage(self.driver)
+        assert editAddressPage.is_title_matches()
+        editAddressPage.firstname = "Marry"
+        editAddressPage.lastname = "Queen"
+        editAddressPage.telephone = "11111111"
+        editAddressPage.address1 = "Viet nam"
+        editAddressPage.zip = "000000"
+        editAddressPage.city = "Viet nam"
+        editAddressPage.select = "Vietnam"
+        
+        editAddressPage.update_address_information()
+
+        assert editAddressPage.is_updated_successfully()
+
+    def test_add_new_address_book(self):
+        accountPage = page.LumaAccountPage(self.driver)
+        assert accountPage.is_title_matches()
+        accountPage.go_to_manage_address_page()
+
+        manageAddressPage = page.LumaManageAddressPage(self.driver)
+        assert manageAddressPage.is_title_matches()
+        manageAddressPage.go_to_create_new_address_page()
+
+        createAddressPage = page.LumaCreateNewAddressPage(self.driver)
+        assert createAddressPage.is_title_matches()
+        createAddressPage.firstname = "Marry" + str(random.randint(1000, 10000))
+        createAddressPage.lastname = "Queen" + str(random.randint(100, 10000))
+        createAddressPage.telephone = str(random.randint(100000000, 900000000))
+        createAddressPage.address1 = "Viet nam"
+        createAddressPage.zip = str(random.randint(100000000, 900000000))
+        createAddressPage.city = "Viet name"
+        createAddressPage.select = "Vietnam"
+
+        createAddressPage.create_new_address()
+
+        assert createAddressPage.is_created_successfully()
+
+    def test_delete_address_book(self):
+        accountPage = page.LumaAccountPage(self.driver)
+        assert accountPage.is_title_matches()
+        accountPage.go_to_manage_address_page()
+
+        manageAddressPage = page.LumaManageAddressPage(self.driver)
+        assert manageAddressPage.is_title_matches()
+        
+        manageAddressPage.delete_address()
+        manageAddressPage.accept_delete_address()
+
+        assert manageAddressPage.is_deleted_successfully()
+
+
+    def tearDown(self):
+        time.sleep(2)
+        self.driver.close()
