5dc2903e449c9fcf33e8bc0fb7cce58a8f968a8d
phamtungthuy <21020412@vnu.edu.vn>
2023-11-14 15:32:23 +0700

    Test cases for adding, removing and purchasing carts Moreover, add test case for reviewing products

--- webdriver/main.py
+++ webdriver/main.py
@@ -2,4 +2,8 @@ from testcase.test import *
 import unittest
 
 if __name__ == "__main__":
-    unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(LumaWebsiteAuthentication))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(LumaControlAccount))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(LumaShopping))
+    unittest.TextTestRunner().run(suite)
--- webdriver/testcase/element.py
+++ webdriver/testcase/element.py
@@ -1,22 +1,21 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+
 class BasePageElement(object):
-    
+    def __init__(self, locator):
+        self.locator = locator
+
     def __set__(self, obj, value):
+        self.driver = obj.driver
         driver = obj.driver
         WebDriverWait(driver, 100).until(
             lambda driver: driver.find_element(By.NAME, self.locator))
         driver.find_element(By.NAME, self.locator).clear()
+
         driver.find_element(By.NAME, self.locator).send_keys(value)
 
-    def __get__(self, obj, owner):
-        driver = obj.driver
-        WebDriverWait(driver, 100).until(
-            lambda driver: driver.find_element(By.NAME, self.locator)
-        )
-        element = driver.find_element(By.NAME, self.locator)
-        return element.get_attribute("value")
     
 class BaseInputElement(object):
     def __init__(self, locator):
@@ -27,6 +26,7 @@ class BaseInputElement(object):
         WebDriverWait(driver, 100).until(
             lambda driver: driver.find_element(By.ID, self.locator)
         )
+        driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'center'});", driver.find_element(By.ID, self.locator))
         driver.find_element(By.ID, self.locator).clear()
         driver.find_element(By.ID, self.locator).send_keys(value)
 
@@ -40,3 +40,20 @@ class BaseSelectElement(object):
             lambda driver : driver.find_element(By.ID, self.locator)
         )
         Select(driver.find_element(By.ID, self.locator)).select_by_visible_text(value)
+
+class BaseSearchElement(object):
+    def __init__(self, locator):
+        self.locator = locator
+
+    def __set__(self, obj, value):
+        self.driver = obj.driver
+        driver = obj.driver
+        WebDriverWait(driver, 100).until(
+            lambda driver: driver.find_element(By.NAME, self.locator))
+        driver.find_element(By.NAME, self.locator).clear()
+        driver.find_element(By.NAME, self.locator).send_keys(value)
+
+    def search(self):
+        WebDriverWait(self.driver, 100).until(
+            lambda driver: driver.find_element(By.NAME, self.locator))
+        self.driver.find_element(By.NAME, self.locator).send_keys(Keys.ENTER)
